91d43e94ff7b460d0ee15de39492f0c9681ec537
anajacobo01 <anali_srg@hotmail.com>
2021-08-11 15:36:26 -0500

    ejercicio operadores

---
+++ main.js
@@ -0,0 +1,99 @@
+
+// console.log("Hola Mundo");
+// alert("Hola mundo desde el navegador");
+
+// // // ES5 ecma Script 5
+// // var nombre = 'Pedro';
+
+// // // ES6
+// // let nombre = 'Pedro';
+// // const nombre = 'Pedro';
+
+// // Datos primitivos 
+
+// // String cadenas de caracteres
+// 'hola mundo'
+// // number  entero, decimales
+// 9
+// 9.08
+
+// // boolean true or false 
+
+// true 
+// false
+// //array
+
+// //object 
+
+// // function 
+
+// // undefined 
+
+// // null
+
+// //typeof regresara el valor 
+
+
+// // coercion 
+// //explicita hace uso de metodos o funciones 
+// let numero = 123;
+// console.log(numero);
+// let conversion = String(numero);
+// console.log(conversion)
+// console.log(numero)
+
+// // implicita 
+
+// let nuevoConversion = numero + '';
+// console.log(nuevoConversion);
+
+// Number ()
+// Boolean ()
+
+// // Operadores logicos y matematicos 
+// // suma
+// 3 + 3 // 6 
+
+// // concatenacion 
+// '12' + 12 // '1212'
+// // resta 
+// 4-6  / -2 
+
+// // multiplicacion 
+
+// 4*4 // 16
+
+// // division 
+// 4 / 2 
+// // modulo 
+
+// 5%3
+
+
+// // incremento 
+// 4++ // 4
+
+// ++4 // 5
+
+
+a = 5
+b = 10
+c = 15
+
+primerEjercicio = (3 + b) * c / a * 2;
+console.log(primerEjercicio);
+
+segundoEjercicio = ((a + b * c) / 5) * 2;
+console.log(segundoEjercicio);
+
+tercerEjercicio = b / a + 2 * c;
+console.log(tercerEjercicio);
+
+cuartoEjercicio  = (a + b + c / c) * a;
+console.log(cuartoEjercicio);
+
+quintoEjercicio  = ((3 * a) / c) + a + b + c;
+console.log(quintoEjercicio);
+
+sextoEjercicio  = a - (b + c) * a / 1 ;
+console.log(sextoEjercicio);
